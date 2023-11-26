ecef35e68bc3fd9fa131ed67c6c51ed49116dbdf
dubiZA <darius@dubiza.io>
2023-11-26 09:52:46 -0700

    Use array of objects for parts

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,27 +1,25 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 ={
-    name: 'State of component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course} />
-      <Content
-        part1={part1}
-        part2={part2}
-        part3={part3}
-      />
-      <Total exercises={part1.exercises + part2.exercises + part3.exercises}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -29,33 +27,33 @@ const App = () => {
 export default App
 
 const Header = (props) => {
-  console.log(props)
+  console.log('Header ', props)
   return (
     <h1>{props.course}</h1>
   )
 }
 
 const Content = (props) => {
-  console.log(props)
+  console.log('Content ', props)
   return (
     <>
-      <Part part={props.part1.name} exercises={props.part1.exercises} />
-      <Part part={props.part2.name} exercises={props.part2.exercises} />
-      <Part part={props.part3.name} exercises={props.part3.exercises} />
+      <Part part={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part part={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part part={props.parts[2].name} exercises={props.parts[2].exercises} />
     </>
   )
 }
 
 const Part = (props) => {
-  console.log(props)
+  console.log('Part ', props)
   return (
     <p>{props.part} {props.exercises}</p>
   )
 }
 
 const Total = (props) => {
-  console.log(props)
+  console.log('Total ', props)
   return (
-    <p>Number of exercises {props.exercises}</p>
+    <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
   )
 }
