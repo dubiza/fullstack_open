b1ec694fe783bbc4687428d52cbd741488eadeab
dubiZA <darius@dubiza.io>
2024-01-04 22:13:05 -0700

    Add check for existing name in phonebook

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,17 +1,30 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]) 
   const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already in the phonebook`)
+    } else {
+      const personObject = {name: newName}
+      setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+  }
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input value={newName} onChange={handleNameChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -19,6 +32,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       ...
+      <div>debug: {newName}</div>
     </div>
   )
 }
