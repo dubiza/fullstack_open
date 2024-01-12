f64dc393c126376161f3f82f7cac686a00e6d7b4
dubiZA <darius@dubiza.io>
2024-01-11 20:46:51 -0700

    Use axios with promises

--- part2/phonebook/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({personFilter, handlePersonFilter}) => {
-    return (
-        <div>
-            filter shown with <input value={personFilter} onChange={handlePersonFilter}/>
-        </div>
-    )
-}
-
-export default Filter
