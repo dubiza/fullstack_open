5c279e0fe876ada1213258b9c4483c588af33218
dubiZA <darius@dubiza.io>
2024-01-17 21:33:18 -0700

    Implement delete button and logic

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -5,73 +5,86 @@ import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
 
 const App = () => {
-    const [persons, setPersons] = useState([])
-    const [newName, setNewName] = useState('')
-    const [newNumber, setNewNumber] = useState('')
-    const [personFilter, setPersonFilter] = useState('')
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [personFilter, setPersonFilter] = useState('')
 
-    useEffect(() => {
+  useEffect(() => {
+    phonebookService
+      .getAll()
+      .then(initialPhonebook => {
+        console.log('setting initial phonebook data')
+        setPersons(initialPhonebook)
+      })
+  }, [])
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+
+  const handlePersonFilter = (event) => {
+    setPersonFilter(event.target.value)
+  }
+
+  const personsToShow = personFilter
+    ? persons.filter(person =>
+      person.name.toLowerCase().includes(personFilter.toLowerCase()))
+    : persons
+
+  const addName = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      alert(`${newName} is already in the phonebook`)
+    } else {
+      const personObject = {
+        name: newName,
+        number: newNumber
+      }
+
       phonebookService
-        .getAll()
+        .create(personObject)
         .then(initialPhonebook => {
-          console.log('setting initial phonebook data')
-          setPersons(initialPhonebook)
+          setPersons(persons.concat(initialPhonebook))
+          setNewName('')
+          setNewNumber('')
         })
-    }, [])
-
-    const handleNameChange = (event) => {
-        setNewName(event.target.value)
     }
+  }
 
-    const handleNumberChange = (event) => {
-        setNewNumber(event.target.value)
+  const removeName = (id, name) => {
+    if (window.confirm(`Delete ${name}?`)) {
+      phonebookService
+      .remove(id)
+      .then(() => {
+        setPersons(persons.filter(person => person.id !== id))
+      })
     }
+  }
 
-    const handlePersonFilter = (event) => {
-        setPersonFilter(event.target.value)
-    }
-
-    const personsToShow = personFilter
-        ? persons.filter(person =>
-            person.name.toLowerCase().includes(personFilter.toLowerCase()))
-        : persons
-
-    const addName = (event) => {
-        event.preventDefault()
-        if (persons.some(person => person.name === newName)) {
-            alert(`${newName} is already in the phonebook`)
-        } else {
-            const personObject = {
-                name: newName,
-                number: newNumber
-            }
-
-            phonebookService
-              .create(personObject)
-              .then(initialPhonebook => {
-                setPersons(persons.concat(initialPhonebook))
-                setNewName('')
-                setNewNumber('')
-              })
-        }
-    }
-
-    return (
-        <div>
-            <h2>Phonebook</h2>
-            <Filter personFilter={personFilter} handlePersonFilter={handlePersonFilter} />
-            <h2>add a new</h2>
-            <PersonForm
-                addName={addName}
-                newName={newName}
-                handleNameChange={handleNameChange}
-                newNumber={newNumber}
-                handleNumberChange={handleNumberChange}
-            />
-            <h2>Numbers</h2>
-            <Persons personsToShow={personsToShow} />
-        </div>
-    )
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Filter personFilter={personFilter} handlePersonFilter={handlePersonFilter} />
+      <h2>add a new</h2>
+      <PersonForm
+        addName={addName}
+        newName={newName}
+        handleNameChange={handleNameChange}
+        newNumber={newNumber}
+        handleNumberChange={handleNumberChange}
+      />
+      <h2>Numbers</h2>
+      <Persons
+        personsToShow={personsToShow}
+        removeName={removeName}
+      />
+    </div>
+  )
 }
 
 export default App
--- part2/phonebook/src/components/Persons.jsx
+++ part2/phonebook/src/components/Persons.jsx
@@ -1,11 +1,14 @@
-const Persons = ({personsToShow}) => {
-    return (
-        <>
-            {personsToShow.map(person =>
-                <div key={person.id}>{person.name} {person.number}</div>
-            )}
-        </>
-    )
+const Persons = ({ personsToShow, removeName }) => {
+  return (
+    <>
+      {personsToShow.map(person =>
+        <div key={person.id}>
+          {person.name} {person.number}
+          <button onClick={() => removeName(person.id, person.name)}>delete</button>
+        </div>
+      )}
+    </>
+  )
 }
 
 export default Persons
